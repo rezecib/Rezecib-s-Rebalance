c5eb7a0abdb27ff3f59d29973c35efd050fe99f6
Rafael Lizarralde <rezecib@gmail.com>
2016-10-29 01:33:49 -0400

    fixed overflow on shadowcreature wantstodespawn

--- scripts/patches/shadowcreatures.lua
+++ scripts/patches/shadowcreatures.lua
@@ -7,26 +7,26 @@ none
 -- if not GLOBAL.TheNet:GetIsServer() then return end
 
 AddStategraphPostInit("shadowcreature", function(sg)
-	local disappear_onenter = sg.states.disappear.onenter
-	function sg.states.disappear.onenter(inst, ...)
-		if inst._recently_attacked then
-			inst.sg:GoToState("idle")
-			return
-		end
-		disappear_onenter(inst, ...)
+	local _idle_onenter = sg.states.idle.onenter
+	sg.states.idle.onenter = function(inst, ...)
+		--It's been flagged for despawning, cache that flag
+		if inst.wantstodespawn then inst._wantstodespawn = inst.wantstodespawn end
+		--Otherwise, only make it want to despawn if it's been flagged before and hasn't been attacked recently
+		inst.wantstodespawn = inst._wantstodespawn and not inst._recently_attacked
+		return _idle_onenter(inst, ...)
 	end
 end)
 
-function RemoveRecentlyAttacked(inst)
+local function RemoveRecentlyAttacked(inst)
 	inst._recently_attackd = nil
 end
 
-function AddRecentlyAttacked(inst)
+local function AddRecentlyAttacked(inst)
 	inst._recently_attacked = true
 	if inst._recently_attacked_task then
 		inst._recently_attacked_task:Cancel()
 	end
-	inst._recently_attacked_task = inst:DoTaskInTime(15, RemoveRecentlyAttacked)
+	inst._recently_attacked_task = inst:DoTaskInTime(100, RemoveRecentlyAttacked)
 end
 
 for _,prefab in pairs({"crawlinghorror", "terrorbeak"}) do
